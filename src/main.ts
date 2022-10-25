f374e5de4a1c5f963c10b73e1769fb6c79e3ce14
z20ist10 <z20ist10@z00p3-15.SPSKNM>
2022-10-25 09:56:57 +0200

    main done

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,32 @@
 import './style.css'
+const stranka=
+`<section id="section1">
+<div class="container">
+  <h1 class="header">CURRENCY EXCHANGE</h1>
+  <div class="main">
+   <div class="Amount">
+    <label for="amount">Amount</label>
+    <input type="text" inputmode="decimal" class="input-amount" id="amount" value="1.00">
+   </div>
+   <div class="Currency1">
+    <label for="from">From</label>
+    <input type="text" class="input-currency1" id="from">
+   </div>
+   <div class="Currency2">
+   <label for="to">To</label>
+    <input type="text" class="input-currency2" id="to">
+   </div>
+  </div>
+  <input class="convert-button" type="button" onclick="alert('Vaša hodnota je:')" value="Convert">
+  <div class="rates">
+   <h1 class="rates1">Live Exchange Rates</h1>
+  </div>
+  
 
+
+</div>
+</section>`
+document.getElementById("app").innerHTML=stranka
 const options = {
 	method: 'GET',
 	headers: {
